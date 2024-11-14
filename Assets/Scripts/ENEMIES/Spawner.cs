485bca674a3e9c5b8076c015a9fa8db0f359aad1
lkSander <lauracuestatorres@gmail.com>
2024-11-14 18:56:22 +0100

    5

--- Assets/Scripts/ENEMIES/Spawner.cs
+++ Assets/Scripts/ENEMIES/Spawner.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+
+    [SerializeField] private GameObject enemigoPrefab;
+    [SerializeField] private Transform[] puntosSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //.position porque busca un vector3
+        //quaternion.identity es 0,0,0
+        Instantiate(enemigoPrefab, puntosSpawn[0].position, Quaternion.identity);
     }
 
     // Update is called once per frame
