945e5da01d519304f6b171c24eb05db6bfa9d64f
lkSander <lauracuestatorres@gmail.com>
2024-11-20 17:02:47 +0100

    bsfht

---
+++ Assets/Scripts/Player/Bazoocaa.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bazoocaa : MonoBehaviour
+{
+
+    [SerializeField] GameObject granadaPrefab;
+    [SerializeField] Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(granadaPrefab, spawnPoint.position, Quaternion.identity);
+
+            // Instantiate(enemigoPrefab, puntosSpawn[Random.Range(0, puntosSpawn.Length)].position, Quaternion.identity);
+        }
+
+
+    }
+}
