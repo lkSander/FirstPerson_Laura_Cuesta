fa05c35a6fedc97eb22d0b1bf34e93821330cdc5
lkSander <lauracuestatorres@gmail.com>
2024-11-21 19:23:12 +0100

    sdhfjkz

--- Assets/Scripts/Player/Granada.cs
+++ Assets/Scripts/Player/Granada.cs
@@ -8,6 +8,11 @@ public class Granada : MonoBehaviour
     Rigidbody rb;
     [SerializeField] private float fuerzaImpulso;
     [SerializeField] private GameObject explosionPrefab;
+
+    [SerializeField] private float radioGranada;
+    [SerializeField] private LayerMask daniableGranada;
+
+    private Collider[] buffer= new Collider[120];
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +26,30 @@ public class Granada : MonoBehaviour
     {
         
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
     private void OnDestroy()
     {
         Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        int explotadosDetectados = Physics.OverlapSphereNonAlloc(transform.position, radioGranada, buffer, daniableGranada);
+        if(explotadosDetectados > 0)
+        {
+            for(int i = 0; i < explotadosDetectados; ++i)
+            {
+                buffer[i].TryGetComponent(out PedroZombie scriptHuesos);
+                //scriptHuesos.Explotar();
+            }
+
+
+        }
+
+    }
+
+    public void Explotar()
+
+    {
+        
     }
 }
